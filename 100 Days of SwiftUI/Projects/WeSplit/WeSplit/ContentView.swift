a9bb4dd7cf635eb4882983bc8389fdfa57e736a8
Carlos Mbendera <carlosmbendera@gmail.com>
2022-10-08 17:44:05 +0200

    Add files via upload

---
+++ 100 Days of SwiftUI/Projects/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,101 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Carlos Mbendera on 26/09/2022.
+//
+
+import SwiftUI
+
+
+
+struct ContentView: View {
+    
+    @FocusState private var amountIsFocused
+    @State private var numOfPeople = 2
+    @State private var checkTotal  = 0.0
+    @State private var tipPercent = 20
+    
+    var userCurrencyType: FloatingPointFormatStyle<Double>.Currency  {
+        .currency(code: Locale.current.currencyCode ?? "USD")
+    }
+    
+    
+    let percentChoices = [5,10,15,20,25,0]
+    
+    var totalCalculatedAmount: Double {
+        let totalNumOfPeople = Double(numOfPeople + 2)
+        let finalPercent = Double(tipPercent) / 100.00
+        
+        let tip = checkTotal * finalPercent
+        let total = tip + checkTotal
+        let amountPerPerson = total/totalNumOfPeople
+        return amountPerPerson
+    }
+    
+    var body: some View {
+       
+            NavigationView {
+                Form{
+                    Section{
+                        TextField("Check Total Amount", value: $checkTotal,format: userCurrencyType)
+                            .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
+                        
+                        
+                        Picker("How many friends are on the table?", selection: $numOfPeople){
+                            ForEach(2..<50){
+                                Text("\($0) Friends")
+                            }
+                        }
+                        
+                    }
+                    
+                    Section{
+                        Picker("Tip Percentage", selection: $tipPercent){
+                            ForEach(0..<101) {
+                                Text($0, format: .percent)
+                            }
+                        }
+                    }
+                header: {
+                    Text("How much tip are you leaving?")
+                }
+                    
+                    Section{
+                        Text(totalCalculatedAmount, format: userCurrencyType)
+                    } header: {
+                        Text("Each should pay:")
+                    }
+                    
+                    Section{
+                        Text(checkTotal * (Double(tipPercent) / 100.00)  + checkTotal, format: userCurrencyType )
+                            .foregroundColor( tipPercent == 0 ? Color.red : Color.primary)
+                    }header: {
+                        Text("Total Without Dividing With Friends")
+                    }
+                }
+                .navigationTitle("WeSplit")
+                .toolbar{
+                    ToolbarItemGroup(placement: .keyboard)
+                    {
+                        Spacer()
+                        Button("Done"){
+                            amountIsFocused = false
+                        }
+                    }
+                }
+                
+            }
+            
+                
+            }
+        }
+    
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
